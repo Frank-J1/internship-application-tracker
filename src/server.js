8c7cf36136284275a90959fb3ba9585ded8092e4
Frank-J1 <frankmartinscnd@gmail.com>
2025-12-30 14:31:06 -0500

    Finished the basic functionality of the app

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const db = require("./db/database");
+
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/ping", (req, res) => {
     res.json({ok: true });
